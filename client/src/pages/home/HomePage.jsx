b49d4792303d1d5686071da135918052abdc346a
Thomas Brockington <xtombrock1989@gmail.com>
2023-12-13 23:54:31 +0000

    save

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -7,17 +7,9 @@ import { ToggleContext } from '../../context/ToggleContext';
 function HomePage() {
   const { setActiveNav } = useContext(ToggleContext);
 
-  let navigate = useNavigate();
-
   useEffect(() => {
     setActiveNav('/');
   }, []);
-
-  const navigateToPage = (event) => {
-    const { id } = event.target;
-    setActiveNav(id);
-    navigate(`${id}`);
-  };
 
   return (
     <div className='grid main__bg font-poppins h-screen grid-rows-reg overflow-hidden max-h-screen'>
@@ -25,17 +17,7 @@ function HomePage() {
       {/* Main */}
       <main className='grid h-full p-1 items-center justify-center'>
         <section>
-          <div className='grid text-center outline outline-2 outline-black rounded-xl bg-yellow-400 px-6 py-8'>
-            <article className=''>
-              <div>
-                <h2 className='text-xl font-semibold'>Welcome To</h2>
-                <h1 className='text-3xl font-poppins font-bold'>
-                  Test Project
-                </h1>
-              </div>
-
-            </article>
-          </div>
+          
         </section>
       </main>
     </div>
